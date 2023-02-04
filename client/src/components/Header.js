a70d6f78ebf1e4b63b463ffeef403cdf2cb83083
“Matthew <matthewlangmuir@hotmail.com>
2023-02-04 18:25:22 -0500

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -67,7 +67,7 @@ const Header = () => {
                             Contact
                         </MobileLink>
                             <Resume
-                                href="https://drive.google.com/file/d/18HMsqpqT9xI11gJEu1Pnum0dwqsah2UD/view?usp=sharing"
+                                href="https://drive.google.com/file/d/1U2_FIiHyVWl6pxQYquR4m5LlKei1BF3a/view?usp=sharing"
                                 target="_blank"
                                 rel="noreferrer"
                                 style={{margin: "30px 0", fontSize: "18px", backgroundColor: "#202124"}}
